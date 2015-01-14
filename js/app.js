7bbdffd479db9e6d1ae570ef79714a1f73485e34
vincenzoauteri <vincenzo.auteri@hotmail.com>
2015-01-14 23:14:22 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
-//Main app code 
+/*
+    
+  Main app code 
 
-//Architecture overwiew: the main class (using pseudoclassical pattern) is Game
-//Game has a World (defining map topology) a Player, an array of Enemy(s) and a 
-//princess which is an instance of the general class Actor for the characters
-//of which Player and Enemy are subclasses
-//
-//Game holds the logic for checking collisions and victory/defeat conditions
-//The only variable in the global scope is game
+  Architecture overwiew: the main class (using pseudoclassical pattern) is Game
+  Game has a World (defining map topology) a Player, an array of Enemy(s) and a 
+  princess which is an instance of the general class Actor for the characters
+  of which Player and Enemy are subclasses
+ 
+  Game holds the logic for checking collisions and victory/defeat conditions 
+  The only variable in the global scope is game
+
+  The code is organized as follows:
+    - app.js: main game object initialization and keyboard input callback
+    - game.js: defines Game class, managing game start and ending, and collision detection
+    - actor.js: defines the generic Actor class for representing characters, and the subclasses
+        Player and Enemy that are specific to those entities.
+    - world.js: defines  World class, which manages map definitions and building.
+    - engine.js basic game engine provided, modified to support the propietary code */
+
 game = new Game();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/* This callback listens for key presses and sends the keys to the
+   Player.handleInput() method.  */
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -18,9 +29,7 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     if (game.running) {
         game.player.handleInput(allowedKeys[e.keyCode]);
     }
-
 });
